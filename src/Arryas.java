9604452372940c0e33ee09f54253fe3054009ec6
prasanth <prasanthreddy513@gmail.com>
2020-08-11 11:20:57 +0530

    secondcode

---
+++ src/Arryas.java
@@ -0,0 +1,35 @@
+
+public class Arryas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a[]= {10,20,30,40,50};
+System.out.println(a[3]);
+
+int b[]=new int[5];
+b[0]=10;
+b[1]=20;
+b[2]=30;
+b[3]=40;
+b[4]=50;
+System.out.println(b[4]);
+
+int c[][]=new int[3][3];
+c[0][0]=110;
+c[0][1]=120;
+c[0][1]=130;
+c[1][0]=140;
+c[1][1]=150;
+c[1][2]=160;
+c[2][0]=170;
+c[2][1]=180;
+c[2][2]=110;
+System.out.println(c[2][2]);
+
+int d[][]= {{21,22,23},{30,31,32},{40,41,42}};
+System.out.println(d[2][1]);
+
+}
+	}
+
+
